469ae575b5c53dbc630eaaeedbd1db8c85700fe8
Samuel Bailey <samuelrbailey19@gmail.com>
2021-02-11 21:54:47 +0000

    Added try catch in case frame does not come through properly

--- udp_server.py
+++ udp_server.py
@@ -3,7 +3,7 @@ import socket
 import pickle
 import numpy as np
 
-host = "127.0.0.1"
+host = "192.168.0.30"
 port = 5000
 max_length = 65540
 
@@ -20,28 +20,31 @@ while True:
     data, address = sock.recvfrom(max_length)
     
     if len(data) < 100:
-        frame_info = pickle.loads(data)
+        try:
+            frame_info = pickle.loads(data)
 
-        if frame_info:
-            nums_of_packs = frame_info["packs"]
+            if frame_info:
+                nums_of_packs = frame_info["packs"]
 
-            for i in range(nums_of_packs):
-                data, address = sock.recvfrom(max_length)
+                for i in range(nums_of_packs):
+                    data, address = sock.recvfrom(max_length)
 
-                if i == 0:
-                    buffer = data
-                else:
-                    buffer += data
+                    if i == 0:
+                        buffer = data
+                    else:
+                        buffer += data
 
-            frame = np.frombuffer(buffer, dtype=np.uint8)
-            frame = frame.reshape(frame.shape[0], 1)
+                frame = np.frombuffer(buffer, dtype=np.uint8)
+                frame = frame.reshape(frame.shape[0], 1)
 
-            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-            frame = cv2.flip(frame, 1)
-            
-            if frame is not None and type(frame) == np.ndarray:
-                cv2.imshow("Receiver", frame)
-                if cv2.waitKey(1) == 27:
-                    break
+                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                frame = cv2.flip(frame, 1)
+                
+                if frame is not None and type(frame) == np.ndarray:
+                    cv2.imshow("Receiver", frame)
+                    if cv2.waitKey(1) == 27:
+                        break
+        except:
+            print("Failed to pickle")
                 
 print("goodbye")
